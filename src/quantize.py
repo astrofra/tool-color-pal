1672f3f462dd729c8e7d39f10dff8eacad3238c0
astrofra <astrofra@gmail.com>
2023-05-28 12:19:23 +0200

    Added MMCQ conversion.

--- src/quantize.py
+++ src/quantize.py
@@ -1,6 +1,7 @@
 import numpy as np
-from operator import itemgetter
-from collections import defaultdict
+from mmcq import MMCQ
+# from operator import itemgetter
+# from collections import defaultdict
 from sklearn.cluster import KMeans
 from collections import Counter
 from PIL import Image
@@ -40,21 +41,6 @@ def build_color_list_from_image(img, progress_callback=progress_callback_stub, p
         return colors
 
 
-# def pre_dither_image(img, luma_amplitude=0.05, progress_callback=progress_callback_stub, pb_min=0, pb_max=100):
-#     img_data = np.array(img)
-#     h, w, _ = img_data.shape
-
-#     for y in range(h):
-#         progress_callback(remap(y / h, 0.0, 1.0, pb_min, pb_max))
-#         for x in range(w):
-#             if (x + y) % 2 == 0:
-#                 img_data[y, x] = np.clip(img_data[y, x] * (1.0 - luma_amplitude), 0, 255)
-#             else:
-#                 img_data[y, x] = np.clip(img_data[y, x] * (1.0 + luma_amplitude), 0, 255)
-
-#     return Image.fromarray(img_data.astype('uint8'))
-
-
 def overlay_formula(a, b):
     return np.where(a <= 0.5, 2 * a * b, 1 - 2 * (1 - a) * (1 - b))
 
@@ -85,9 +71,56 @@ def quantize_colors(colors, n_colors=16, method="kmeans"):
         return quantize_colors_median_cut(colors, n_colors)
     if method.lower() == "popularity":
         return quantize_colors_popularity(colors, n_colors)
+    if method.lower() == "mmcq":
+        return quantize_colors_mmcq(colors, n_colors)
 
 
-from collections import Counter
+def halve_palette(colors, num_clusters=None):
+    # Convert list to numpy array
+    colors = np.array(colors)
+
+    # If num_clusters is not given, cut the palette size in half
+    if num_clusters is None:
+        num_clusters = len(colors) // 2
+
+    # Fit a k-means model
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(colors)
+
+    # Get the representative colors
+    new_colors = kmeans.cluster_centers_.astype(int)
+
+    # Return as a list
+    return new_colors.tolist()
+
+
+def quantize_colors_mmcq(colors, n_colors=16):
+    # Normalize the colors
+    colors = np.array(colors, dtype=np.int32)
+    colors = [tuple(x) for x in colors]
+
+    # Initialize the list of representative colors
+    representative_colors = []
+
+    # Same loop as before, to handle possible color duplication due to quantization
+    requested_colors = n_colors * 2
+    while len(representative_colors) < n_colors:
+        # Select the most common colors
+        representative_colors = np.array(MMCQ.quantize(colors, requested_colors).palette)
+
+        # Apply the same quantization
+        representative_colors = np.round(np.array(representative_colors) / 17) * 17
+        representative_colors = representative_colors.astype(int)
+
+        # Remove duplicates
+        representative_colors = np.unique(representative_colors, axis=0)
+        representative_colors = representative_colors.tolist()
+
+        requested_colors += 1
+
+    representative_colors = halve_palette(representative_colors)
+
+    return representative_colors
+
 
 def quantize_colors_popularity(colors, n_colors=16):
     # Normalize the colors
@@ -117,7 +150,6 @@ def quantize_colors_popularity(colors, n_colors=16):
 
     return representative_colors
 
-
 def quantize_colors_median_cut(colors, n_colors=16):
     # Normalize the colors
     colors = np.array(colors, dtype=np.int32)
@@ -130,26 +162,27 @@ def quantize_colors_median_cut(colors, n_colors=16):
 
     # Split the buckets until we have at least n_colors unique representative colors
     while len(representative_colors) < n_colors:
-        # Find the longest color dimension in the bucket
-        longest_dim = max(range(3), key=lambda dim: max(colors[:,dim]) - min(colors[:,dim]))
-
         # Select the bucket to split
         # In this case, we choose the bucket with the most colors
         largest_bucket_index = max(range(len(color_buckets)), key=lambda index: len(color_buckets[index]))
-        largest_bucket = color_buckets[largest_bucket_index]
+        largest_bucket = np.array(color_buckets[largest_bucket_index])
 
-        # Sort the selected bucket along the longest dimension
-        largest_bucket.sort(key=itemgetter(longest_dim))
+        # Find the color dimension with the greatest standard deviation in the bucket
+        std_dev = np.std(largest_bucket, axis=0)
+        highest_var_dim = np.argmax(std_dev)
 
-        # Find the median color along the longest dimension
+        # Sort the selected bucket along the color dimension with the greatest standard deviation
+        largest_bucket = largest_bucket[largest_bucket[:,highest_var_dim].argsort()]
+
+        # Find the median color along the color dimension with the greatest standard deviation
         median_index = len(largest_bucket) // 2
 
         # Split the bucket into two around the median color
-        color_buckets[largest_bucket_index] = largest_bucket[:median_index]
-        color_buckets.append(largest_bucket[median_index:])
+        color_buckets[largest_bucket_index] = largest_bucket[:median_index].tolist()
+        color_buckets.append(largest_bucket[median_index:].tolist())
 
         # Calculate the representative colors
-        representative_colors_temp = [np.mean(bucket, axis=0) for bucket in color_buckets]
+        representative_colors_temp = [np.mean(np.array(bucket), axis=0) for bucket in color_buckets]
 
         # Apply the same quantization
         representative_colors_temp = np.round(np.array(representative_colors_temp) / 17) * 17
